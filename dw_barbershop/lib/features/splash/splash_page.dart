9a27ca5e2fa49a889e657ed6eec1391a337dfacc
murilo <61051754+muriloamancio00@users.noreply.github.com>
2023-08-15 10:13:38 -0300

    page structure, opaque state Logo

--- dw_barbershop/lib/features/splash/splash_page.dart
+++ dw_barbershop/lib/features/splash/splash_page.dart
@@ -1,14 +1,51 @@
 import 'package:flutter/material.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
 
-  const SplashPage({ super.key });
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
 
-   @override
-   Widget build(BuildContext context) {
-       return Scaffold(
-           appBar: AppBar(title: const Text('Splash'),),
-           body: Container(),
-       );
+class _SplashPageState extends State<SplashPage> {
+
+  var _scale =10;
+  // quando a tela é construida ela vem sem cor, e quando constroi ela recebe o valor se alterando
+  var _animationOpacityLogo =0.0;
+
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((_) { 
+      setState(() {
+        _animationOpacityLogo = 1.0;
+      });
+    });
+    super.initState();
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: DecoratedBox(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(
+                'assets/images/background_image_chair.jpg',
+              ),
+              opacity: 0.2,
+              fit: BoxFit.cover
+            ),
+          ),
+          child: Center(
+            child: AnimatedOpacity(
+              duration: const Duration(seconds: 3),
+              curve: Curves.easeIn,
+              opacity: _animationOpacityLogo,
+              child: Image.asset('assets/images/imgLogo.png')
+            ),
+          ),
+      ),
+    );
   }
 }
