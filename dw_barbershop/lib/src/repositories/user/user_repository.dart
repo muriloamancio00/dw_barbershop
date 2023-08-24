25aac1fddd140894c9c533c2bdd26c36fa7c9110
murilo <61051754+muriloamancio00@users.noreply.github.com>
2023-08-24 15:08:16 -0300

    implements register profile ADM,EMPLOYEE

--- dw_barbershop/lib/src/repositories/user/user_repository.dart
+++ dw_barbershop/lib/src/repositories/user/user_repository.dart
@@ -5,19 +5,34 @@ import 'package:dw_barbershop/src/core/fp/nil.dart';
 import 'package:dw_barbershop/src/model/user_model.dart';
 
 abstract interface class UserRepository {
-
-  Future<Either<AuthException, String>> login(String email, String password); 
+  Future<Either<AuthException, String>> login(String email, String password);
 
   Future<Either<RepositoryException, UserModel>> me();
 
 //(({{recordNomeado}}))
-  Future<Either<RepositoryException, Nil>> registerAdmin(({
-    String name,
-    String email,
-    String password,
-  }) userData);
+  Future<Either<RepositoryException, Nil>> registerAdmin(
+      ({
+        String name,
+        String email,
+        String password,
+      }) userData);
 
   Future<Either<RepositoryException, List<UserModel>>> getEmployees(
       int barbershopId);
 
+  Future<Either<RepositoryException, Nil>> registerAdmAsEmployee(
+      ({
+        List<String> workDays,
+        List<int> workHours,
+      }) userModel);
+
+  Future<Either<RepositoryException, Nil>> registerEmployee(
+      ({
+        int barbershopId,
+        String name,
+        String email,
+        String password,
+        List<String> workDays,
+        List<int> workHours,
+      }) userModel);
 }
--- dw_barbershop/lib/src/repositories/user/user_repository_impl.dart
+++ dw_barbershop/lib/src/repositories/user/user_repository_impl.dart
@@ -95,4 +95,62 @@ class UserRepositoryImpl implements UserRepository {
           RepositoryException(message: 'Erro ao buscar colaboradores'));
     }
   }
+
+  @override
+  Future<Either<RepositoryException, Nil>> registerAdmAsEmployee(
+      ({List<String> workDays, List<int> workHours}) userModel) async {
+    try {
+      final userModelResult = await me();
+
+      final int userId;
+
+      switch (userModelResult) {
+        case Success(value: UserModel(:var id)):
+          userId = id;
+        case Failure(:var exception):
+          return Failure(exception);
+      }
+      await restClient.auth.put('/users/$userId', data: {
+        'work_days': userModel.workDays,
+        'work_hours': userModel.workHours,
+      });
+
+      return Success(nil);
+    } on DioException catch (e, s) {
+      log('Erro ao inserir administrador como colaborador',
+          error: e, stackTrace: s);
+      return Failure(RepositoryException(
+          message: 'Erro ao inserir administrador como colaborador'));
+    }
+  }
+
+  @override
+  Future<Either<RepositoryException, Nil>> registerEmployee(
+      ({
+        int barbershopId,
+        String email,
+        String name,
+        String password,
+        List<String> workDays,
+        List<int> workHours
+      }) userModel) async {
+    try {
+      await restClient.auth.post('/users/', data: {
+        'name': userModel.name,
+        'email': userModel.email,
+        'password': userModel.password,
+        'barbershop_id': userModel.barbershopId,
+        'profile': 'EMPLOYEE',
+        'work_days': userModel.workDays,
+        'work_hours': userModel.workHours,
+      });
+
+      return Success(nil);
+    } on DioException catch (e, s) {
+      log('Erro ao inserir administrador como colaborador',
+          error: e, stackTrace: s);
+      return Failure(RepositoryException(
+          message: 'Erro ao inserir administrador como colaborador'));
+    }
+  }
 }
