d8ec972c979e4ec9d857fe4e0b3af22430b56677
murilo <61051754+muriloamancio00@users.noreply.github.com>
2023-08-25 17:30:40 -0300

    home buttons schedule and view schedule

--- dw_barbershop/lib/src/features/home/employee/home_employee_page.dart
+++ dw_barbershop/lib/src/features/home/employee/home_employee_page.dart
@@ -1,93 +1,115 @@
+import 'package:dw_barbershop/src/core/providers/aplication_providers.dart';
 import 'package:dw_barbershop/src/core/ui/constants.dart';
 import 'package:dw_barbershop/src/core/ui/widgets/avatar_widget.dart';
+import 'package:dw_barbershop/src/core/ui/widgets/barbershop_loader.dart';
 import 'package:dw_barbershop/src/features/home/widgets/home_header.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class HomeEmployeePage extends StatelessWidget {
+import '../../../model/user_model.dart';
+
+class HomeEmployeePage extends ConsumerWidget {
   const HomeEmployeePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final userModelAsync = ref.watch(getMeProvider);
+
     return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          const SliverToBoxAdapter(
-            child: HomeHeader(
-              hideFilter: true,
-            ),
-          ),
-          SliverFillRemaining(
-            hasScrollBody: false,
-            child: Padding(
-              padding: const EdgeInsets.all(24.0),
-              child: Column(
-                children: [
-                  const AvatarWidget(
-                    hideUploadButton: true,
-                  ),
-                  const SizedBox(
-                    height: 24,
-                  ),
-                  const Text(
-                    'Nome e Sobrenome',
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                  Container(
-                    width: MediaQuery.sizeOf(context).width * .7,
-                    height: 108,
-                    decoration: BoxDecoration(
-                      border: Border.all(color: ColorsConstants.grey),
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                    child: const Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          '5',
-                          style: TextStyle(
-                            fontSize: 32,
-                            color: ColorsConstants.brow,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                        Text(
-                          'Hoje',
-                          style: TextStyle(
-                            fontSize: 14,
-                            fontWeight: FontWeight.w500,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 24,
-                  ),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      minimumSize: const Size.fromHeight(56),
-                    ),
-                    onPressed: () {},
-                    child: const Text('AGENDAR CLIENTE'),
-                  ),
-                  const SizedBox(
-                     height: 24,
-                  ),
-                  OutlinedButton(
-                    style: OutlinedButton.styleFrom(
-                      minimumSize: const Size.fromHeight(56),
-                    ),
-                    onPressed: () {},
-                    child: const Text('VER AGENDA'),
-                  )
-                ],
+      body: userModelAsync.when(
+        error: (error, stackTrace) {
+          return const Center(child: Text('Erro ao carregar página'));
+        },
+        loading: () => const BarbershopLoader(),
+        data: (user) {
+          final UserModel(:name) = user;
+          return CustomScrollView(
+            slivers: [
+              const SliverToBoxAdapter(
+                child: HomeHeader(
+                  hideFilter: true,
+                ),
               ),
-            ),
-          ),
-        ],
+              SliverFillRemaining(
+                hasScrollBody: false,
+                child: Padding(
+                  padding: const EdgeInsets.all(24.0),
+                  child: Column(
+                    children: [
+                      const AvatarWidget(
+                        hideUploadButton: true,
+                      ),
+                      const SizedBox(
+                        height: 24,
+                      ),
+                      Text(
+                        name,
+                        style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      Container(
+                        width: MediaQuery.sizeOf(context).width * .7,
+                        height: 108,
+                        decoration: BoxDecoration(
+                          border: Border.all(color: ColorsConstants.grey),
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        child: const Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              '5',
+                              style: TextStyle(
+                                fontSize: 32,
+                                color: ColorsConstants.brow,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                            Text(
+                              'Hoje',
+                              style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 24,
+                      ),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          minimumSize: const Size.fromHeight(56),
+                        ),
+                        onPressed: () {
+                          Navigator.of(context)
+                              .pushNamed('/schedule', arguments: user);
+                        },
+                        child: const Text('AGENDAR CLIENTE'),
+                      ),
+                      const SizedBox(
+                        height: 24,
+                      ),
+                      OutlinedButton(
+                        style: OutlinedButton.styleFrom(
+                          minimumSize: const Size.fromHeight(56),
+                        ),
+                        onPressed: () {
+                          Navigator.of(context).pushNamed('/employee/schedule',
+                              arguments: user);
+                        },
+                        child: const Text('VER AGENDA'),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          );
+        },
       ),
     );
   }
